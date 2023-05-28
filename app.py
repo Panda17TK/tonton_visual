d5b8ef3771f446a60688100fd7bd0c28bba18dbd
Panda17TK <bantian.tech@gmail.com>
2023-05-28 10:47:54 +0900

    add main and app

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import streamlit as st
+from flask import Flask, render_template, request, jsonify, redirect
 import sys
 import json
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from SetData import setdata
-

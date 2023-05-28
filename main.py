b98c11e3aaa7e84d9cdc719175aeb899881c7efd
Panda17TK <bantian.tech@gmail.com>
2023-05-29 00:32:13 +0900

    add select_box

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import json
 import os
 import pandas as pd
+import  streamlit_toggle as tog
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from SetData import setdata
@@ -10,14 +12,30 @@ from SetData import setdata
 
 def result_page():
     # ページ2の内容
-    st.title('結果の表示')
+    st.title('特定の人の予定を抽出')
     input_value = st.session_state.get('input_value')
     st.write('取得したURL:', input_value)
     st.session_state.setdata = setdata.SetData(input_value)
-    for name in st.session_state.setdata.data_user_frame.keys():
-        st.header(name)
-        st.session_state.setdata.data_user_frame[name]=st.session_state.setdata.data_user_frame[name].replace("", pd.NA).dropna(how='all', axis=0)
-        st.write(st.session_state.setdata.data_user_frame[name])
+    stock = st.selectbox(label="絞り込みする人を選んでください",
+            options=list(st.session_state.setdata.data_user_frame.keys()))
+    st.header(stock)
+    st.session_state.setdata.data_user_frame[stock]=st.session_state.setdata.data_user_frame[stock].replace("", pd.NA).dropna(how='all', axis=0)
+    key1 = tog.st_toggle_switch(label="転置",
+                    key="Key1",
+                    default_value=False,
+                    label_after = True,
+                    inactive_color = '#D3D3D3',
+                    active_color="#11567f",
+                    track_color="#29B5E8"
+                    )
+    if key1 == False:
+        st.dataframe(st.session_state.setdata.data_user_frame[stock])
+    elif key1 == True:
+        st.dataframe(st.session_state.setdata.data_user_frame[stock].T)
+    #for name in st.session_state.setdata.data_user_frame.keys():
+    #    st.header(name)
+    #    st.session_state.setdata.data_user_frame[name]=st.session_state.setdata.data_user_frame[name].replace("", pd.NA).dropna(how='all', axis=0)
+    #    st.write(st.session_state.setdata.data_user_frame[name])
 
     if st.button('URL入力画面に戻る'):
         # ページ2に遷移する際に入力値を渡す
